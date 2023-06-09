ad3602f9c501c66505235c1c2ce78967d8aaddc0
Matheus Oliveira <mhoa.2000@gmail.com>
2023-06-09 16:06:29 -0300

    classes para acessar o repositorio usuario

---
+++ src/Backend/MeuLivroDeReceitas.Domain/Entidades/Usuario.cs
@@ -0,0 +1,10 @@
+﻿namespace MeuLivroDeReceitas.Domain.Entidades;
+
+public class Usuario : EntidadeBase
+{
+    public string Nome { get; set; }
+    public string Email { get; set; }
+    public string Telefone { get; set; }
+    public string Senha { get; set; }
+
+}
