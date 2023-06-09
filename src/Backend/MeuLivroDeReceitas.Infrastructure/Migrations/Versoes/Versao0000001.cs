17b9f676511d59bb5c70abb0574ca8c41585d845
Matheus Oliveira <mhoa.2000@gmail.com>
2023-06-08 21:02:53 -0300

    adicionando as classes de migration da tabela usuario no banco de dados

---
+++ src/Backend/MeuLivroDeReceitas.Infrastructure/Migrations/Versoes/Versao0000001.cs
@@ -0,0 +1,23 @@
+﻿using FluentMigrator;
+
+namespace MeuLivroDeReceitas.Infrastructure.Migrations.Versoes;
+
+[Migration((long)NumeroVersoes.CriaTabelaUsuario,"Cria tabela usuario")]
+
+public class Versao0000001 : Migration
+{
+    public override void Down()
+    {
+    }
+
+    public override void Up()
+    {
+        var tabela =  VersaoBase.InserirColunasPadrao(Create.Table("Usuario"));
+        tabela
+            .WithColumn("Nome").AsString(100).NotNullable()
+            .WithColumn("Email").AsString(100).NotNullable()
+            .WithColumn("Senha").AsString(2000).NotNullable()
+            .WithColumn("Telefone").AsString(14).NotNullable();
+
+    }
+}
