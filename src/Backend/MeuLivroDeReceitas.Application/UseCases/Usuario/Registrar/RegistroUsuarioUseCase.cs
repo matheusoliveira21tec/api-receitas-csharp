09ae90e630ba4ab1bce09c23bb4bc28c1ce1db66
Matheus Oliveira <mhoa.2000@gmail.com>
2023-06-14 12:38:15 -0300

    adicionando rn para email repetido

--- src/Backend/MeuLivroDeReceitas.Application/UseCases/Usuario/Registrar/RegistroUsuarioUseCase.cs
+++ src/Backend/MeuLivroDeReceitas.Application/UseCases/Usuario/Registrar/RegistroUsuarioUseCase.cs
@@ -4,33 +4,36 @@ using MeuLivroDeReceitas.Application.Services.Token;
 using MeuLivroDeReceitas.Comunicacao.Request;
 using MeuLivroDeReceitas.Comunicacao.Response;
 using MeuLivroDeReceitas.Domain.Repositorios;
+using MeuLivroDeReceitas.Exception;
 using MeuLivroDeReceitas.Exception.ExceptionBase;
 
 namespace MeuLivroDeReceitas.Application.UseCases.Usuario.Registrar;
 
 public class RegistroUsuarioUseCase : IRegistroUsuarioUseCase
 {
-    private readonly IUsuarioWriteOnlyRepositorio _repo;
+    private readonly IUsuarioWriteOnlyRepositorio _repoWrite;
+    private readonly IUsuarioReadOnlyRepositorio _repoRead;
     private readonly IMapper _mapper;
     private readonly IUnidadeDeTrabalho _unidadeDeTrabalho;
     private readonly EncriptadorDeSenha _encriptadorDeSenha;
     private readonly TokenController _tokenController;
 
-    public RegistroUsuarioUseCase(IUsuarioWriteOnlyRepositorio repo, IMapper mapper, IUnidadeDeTrabalho unidadeDeTrabalho, EncriptadorDeSenha encriptadorDeSenha, TokenController tokenController)
+    public RegistroUsuarioUseCase(IUsuarioWriteOnlyRepositorio repoWrite, IMapper mapper, IUnidadeDeTrabalho unidadeDeTrabalho, EncriptadorDeSenha encriptadorDeSenha, TokenController tokenController, IUsuarioReadOnlyRepositorio repoRead)
     {
-        _repo = repo;
+        _repoWrite = repoWrite;
         _mapper = mapper;
         _unidadeDeTrabalho = unidadeDeTrabalho;
         _encriptadorDeSenha = encriptadorDeSenha;
         _tokenController = tokenController;
+        _repoRead = repoRead;   
     }
     public async Task<ResponseUsuarioRegistradoJson> Executar(RequestRegistrarUsuarioJson request)
     {
-        Validar(request);
+        await Validar(request);
 
         var entidade = _mapper.Map<Domain.Entidades.Usuario>(request);
         entidade.Senha = _encriptadorDeSenha.Criptografar(request.Senha);
-        await _repo.Adicionar(entidade);
+        await _repoWrite.Adicionar(entidade);
         await _unidadeDeTrabalho.Commit();
         var token = _tokenController.GerarToken(entidade.Email);
         return new ResponseUsuarioRegistradoJson
@@ -39,12 +42,18 @@ public class RegistroUsuarioUseCase : IRegistroUsuarioUseCase
         };
     }
 
-    private void Validar(RequestRegistrarUsuarioJson request)
+    private async Task Validar(RequestRegistrarUsuarioJson request)
     {
        var validator = new RegistroUsuarioValidator();
        var result  = validator.Validate(request);
 
-        if (!result.IsValid)
+       var existeUsuariocomEmail = await  _repoRead.ExisteUsuarioComEmail(request.Email);
+       if (existeUsuariocomEmail)
+       {
+            result.Errors.Add(new FluentValidation.Results.ValidationFailure("email", ResourceErrorMessage.EMAIL_JA_CADASTRADO));
+       }
+
+       if (!result.IsValid)
         {
             var errorMessages = result.Errors.Select(error => error.ErrorMessage).ToList();
             throw new ErrosDeValidacaoException(errorMessages);
