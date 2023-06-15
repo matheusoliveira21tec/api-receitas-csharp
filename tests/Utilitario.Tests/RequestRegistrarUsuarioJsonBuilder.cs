fa9eea5b5b2012450b7a1a7853f504b58abf9564
Matheus Oliveira <mhoa.2000@gmail.com>
2023-06-15 14:10:45 -0300

    testes unitarios + postamn collection

---
+++ tests/Utilitario.Tests/RequestRegistrarUsuarioJsonBuilder.cs
@@ -0,0 +1,16 @@
+﻿using Bogus;
+using MeuLivroDeReceitas.Comunicacao.Request;
+
+namespace Utilitario.Tests;
+
+public class RequestRegistrarUsuarioJsonBuilder
+{
+    public static RequestRegistrarUsuarioJson Construir(int tamanhoSenha = 10)
+    {
+        return new Faker<RequestRegistrarUsuarioJson>()
+            .RuleFor(r => r.Nome, f => f.Person.FullName)
+            .RuleFor(r => r.Email, f => f.Internet.Email())
+            .RuleFor(r => r.Senha, f => f.Internet.Password(tamanhoSenha))
+            .RuleFor(r => r.Telefone, f => f.Phone.PhoneNumber("## ! ####-####").Replace("!", $"{f.Random.Int(min: 1, max: 9)}"));
+    }
+}
